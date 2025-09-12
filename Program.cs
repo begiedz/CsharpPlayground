83757c33922e1456ac12a5a6a8f0d702a8c80680
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-09-12 18:53:21 +0200

    feat: add Dictionary and HashSet

--- Program.cs
+++ Program.cs
@@ -31,5 +31,34 @@ class Program
 		coffee = new SugarDecorator(coffee);
 		Console.WriteLine(coffee.GetDescription());
 		Console.WriteLine(coffee.GetCost());
-	}
+
+		// Dictionary (HashMap, frequency map)
+        var dict = new Dictionary<string, int>();
+		dict["apple"] = 3;
+        dict["banana"] = 5;
+
+		foreach (var keyValue in dict)
+			Console.WriteLine($"{keyValue.Key} -> {keyValue.Value}");
+
+		// ===
+		string[] items = { "apple", "banana", "apple", "pear", "banana", "banana" };
+
+        var counts = new Dictionary<string, int>();
+        foreach (var item in items)
+            counts[item] = counts.TryGetValue(item, out var count) ? count + 1 : 1;
+
+        Console.WriteLine("Counts:");
+        foreach (var pair in counts)
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+
+        // HashSet
+        var set = new HashSet<int>();
+		set.Add(10);
+        set.Add(20);
+
+		foreach (var entry in set )
+			Console.WriteLine(entry);
+
+       
+    }
 }
